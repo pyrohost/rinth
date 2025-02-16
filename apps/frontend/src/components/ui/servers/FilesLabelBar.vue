84609928bd39812b7ecca89a0006a425c70c0173
he3als <65787561+he3als@users.noreply.github.com>
2025-02-16 00:38:38 +0000

    feat(files): improve column styling

--- apps/frontend/src/components/ui/servers/FilesLabelBar.vue
+++ apps/frontend/src/components/ui/servers/FilesLabelBar.vue
@@ -1,52 +1,58 @@
 <template>
   <div
     aria-hidden="true"
-    class="sticky top-12 z-20 flex h-8 w-full select-none flex-row items-center border-0 border-b border-solid border-bg-raised bg-bg px-3 text-xs font-bold uppercase"
+    class="sticky top-12 z-20 hidden h-8 w-full select-none flex-row items-center border-0 border-b border-solid border-bg-raised bg-bg text-xs sm:flex"
   >
-    <div class="min-w-[48px]"></div>
     <button
-      class="flex h-full w-full appearance-none items-center gap-1 bg-transparent text-left hover:text-brand"
+      :class="[
+        'mr-2 flex h-full w-full appearance-none items-center gap-1 bg-transparent px-0 text-left text-secondary',
+        { 'font-semibold !text-contrast': sortField === 'name' },
+      ]"
       @click="$emit('sort', 'name')"
     >
+      <div class="min-w-[56px]"></div>
       <span>Name</span>
       <ChevronUpIcon v-if="sortField === 'name' && !sortDesc" class="h-3 w-3" aria-hidden="true" />
       <ChevronDownIcon v-if="sortField === 'name' && sortDesc" class="h-3 w-3" aria-hidden="true" />
     </button>
-    <div class="flex shrink-0 gap-4 text-right md:gap-12">
-      <button
-        class="hidden min-w-[160px] appearance-none items-center justify-start gap-1 bg-transparent hover:text-brand md:flex"
-        @click="$emit('sort', 'created')"
-      >
-        <span>Created</span>
-        <ChevronUpIcon
-          v-if="sortField === 'created' && !sortDesc"
-          class="h-3 w-3"
-          aria-hidden="true"
-        />
-        <ChevronDownIcon
-          v-if="sortField === 'created' && sortDesc"
-          class="h-3 w-3"
-          aria-hidden="true"
-        />
-      </button>
-      <button
-        class="mr-4 hidden min-w-[160px] appearance-none items-center justify-start gap-1 bg-transparent hover:text-brand md:flex"
-        @click="$emit('sort', 'modified')"
-      >
-        <span>Modified</span>
-        <ChevronUpIcon
-          v-if="sortField === 'modified' && !sortDesc"
-          class="h-3 w-3"
-          aria-hidden="true"
-        />
-        <ChevronDownIcon
-          v-if="sortField === 'modified' && sortDesc"
-          class="h-3 w-3"
-          aria-hidden="true"
-        />
-      </button>
-      <div class="min-w-[24px]"></div>
-    </div>
+    <button
+      :class="[
+        'mr-2 hidden h-full min-w-[200px] appearance-none items-center justify-start gap-1 bg-transparent px-0 text-secondary md:flex',
+        { 'font-semibold !text-contrast': sortField === 'created' },
+      ]"
+      @click="$emit('sort', 'created')"
+    >
+      <span>Created</span>
+      <ChevronUpIcon
+        v-if="sortField === 'created' && !sortDesc"
+        class="h-3 w-3"
+        aria-hidden="true"
+      />
+      <ChevronDownIcon
+        v-if="sortField === 'created' && sortDesc"
+        class="h-3 w-3"
+        aria-hidden="true"
+      />
+    </button>
+    <button
+      :class="[
+        'flex h-full min-w-[223px] appearance-none items-center justify-start gap-1 bg-transparent px-0 text-secondary md:min-w-[256px]',
+        { 'font-semibold !text-contrast': sortField === 'modified' },
+      ]"
+      @click="$emit('sort', 'modified')"
+    >
+      <span>Modified</span>
+      <ChevronUpIcon
+        v-if="sortField === 'modified' && !sortDesc"
+        class="h-3 w-3"
+        aria-hidden="true"
+      />
+      <ChevronDownIcon
+        v-if="sortField === 'modified' && sortDesc"
+        class="h-3 w-3"
+        aria-hidden="true"
+      />
+    </button>
   </div>
 </template>
 
